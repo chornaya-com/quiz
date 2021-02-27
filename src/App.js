43d3b4d38ea434f916791211fbefeb0c0d190bac
the-chornaya <me@chornaya.com>
2021-02-27 18:58:36 +0000

    fetch data from the backend

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@ import React from 'react';
 import {MainPage} from './mainPage/MainPage';
 import {QuestionPage} from './questionPage/QuestionPage';
 import {ResultPage} from './resultPage/ResultPage';
-import questionsResponse from './data.json';
+import axios from 'axios';
 
 export const AppContext = React.createContext();
 
 function App() {
-    const questions = questionsResponse.results;
+    const url = 'https://opentdb.com/api.php?amount=15&category=22&difficulty=easy';
+    const [questionCards, setQuestionCards] = React.useState([]);
     const [activePage, setActivePage] = React.useState('MainPage');
     const [activeQuestionIndex, setActiveQuestionIndex] = React.useState(0);
-    const [activeQuestion, setActiveQuestion] = React.useState(questions[0]);
+    const [activeQuestion, setActiveQuestion] = React.useState(questionCards[0]);
     const [score, setScore] = React.useState(0);
     const isMainPage = activePage === 'MainPage';
     const isQuestionPage = activePage === 'QuestionPage';
     const isResultPage = activePage === 'ResultPage';
 
+    React.useEffect(() => {
+        axios
+            .get(url)
+            .then((response) => {
+                setQuestionCards(response.data.results);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
     const startGame = () => {
         setActivePage('QuestionPage');
         setActiveQuestionIndex(0);
-        setActiveQuestion(questions[0]);
+        setActiveQuestion(questionCards[0]);
     };
 
     const nextQuestion = () => {
         const nextQuestionIndex = activeQuestionIndex + 1;
-        if (nextQuestionIndex < 10) {
+        if (nextQuestionIndex < 15) {
             setActiveQuestionIndex(nextQuestionIndex);
-            setActiveQuestion(questions[nextQuestionIndex]);
+            setActiveQuestion(questionCards[nextQuestionIndex]);
         } else {
             setActivePage('ResultPage');
         }
@@ -37,7 +49,7 @@ function App() {
     };
 
     const contextValue = {
-        numberOfQuestions: questions.length,
+        numberOfQuestions: questionCards.length,
         startGame,
         activeQuestion,
         activeQuestionIndex,
